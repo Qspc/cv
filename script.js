5d59cdf5603d9f5d3536c7a5e82b931684debdee
Qspc <shibgotalloh@gmail.com>
2025-07-14 13:43:46 +0700

    update: copywriting to english

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
 // efek navbar scroll + button auto scroll atas
-const header = document.querySelector('.navbar');
-const naik = document.querySelector('.kotak');
+const header = document.querySelector(".navbar");
+const naik = document.querySelector(".kotak");
 
 window.onscroll = function () {
-  var top = window.scrollY;
-  // console.log(top);
-  if (top >= 100) {
-    header.classList.add('sticky');
-    naik.classList.add('scroll');
-  } else {
-    header.classList.remove('sticky');
-    naik.classList.remove('scroll');
-  }
+    var top = window.scrollY;
+    // console.log(top);
+    if (top >= 100) {
+        header.classList.add("sticky");
+        naik.classList.add("scroll");
+    } else {
+        header.classList.remove("sticky");
+        naik.classList.remove("scroll");
+    }
 };
 
 // efek typing pada home
-const typing = document.querySelector('.typing');
-const ListArray = ['Information Enthusiast', 'Agent Moslem', 'Frontend Developer'];
+const typing = document.querySelector(".typing");
+const ListArray = ["Information Enthusiast", "Frontend Developer"];
 const time = 100;
 let arrayIndex = 0;
 let charIndex = 0;
 
 function type() {
-  if (charIndex < ListArray[arrayIndex].length) {
-    typing.textContent += ListArray[arrayIndex].charAt(charIndex);
-    charIndex++;
-    setTimeout(type, time);
-  } else {
-    setTimeout(erase, time + 2000);
-  }
+    if (charIndex < ListArray[arrayIndex].length) {
+        typing.textContent += ListArray[arrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, time);
+    } else {
+        setTimeout(erase, time + 2000);
+    }
 }
 function erase() {
-  if (charIndex > 0) {
-    typing.textContent = ListArray[arrayIndex].substring(0, charIndex - 1);
-    charIndex--;
-    setTimeout(erase, time - 50);
-  } else {
-    arrayIndex++;
-    if (arrayIndex >= ListArray.length) {
-      arrayIndex = 0;
+    if (charIndex > 0) {
+        typing.textContent = ListArray[arrayIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase, time - 50);
+    } else {
+        arrayIndex++;
+        if (arrayIndex >= ListArray.length) {
+            arrayIndex = 0;
+        }
+        setTimeout(type, time + 1000);
     }
-    setTimeout(type, time + 1000);
-  }
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  type(type, time + 2000);
+document.addEventListener("DOMContentLoaded", function () {
+    type(type, time + 2000);
 });
 
 // $(document).ready(function(){
@@ -107,24 +107,26 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // https://colorlib.com/preview/theme/seogo/
 
-const buttonsWrapper = document.querySelector('.map');
-const slides = document.querySelector('.inner');
+const buttonsWrapper = document.querySelector(".map");
+const slides = document.querySelector(".inner");
 
-buttonsWrapper.addEventListener('click', (e) => {
-  if (e.target.nodeName === 'BUTTON') {
-    Array.from(buttonsWrapper.children).forEach((item) => item.classList.remove('active'));
-    if (e.target.classList.contains('first')) {
-      slides.style.transform = 'translateX(-0%)';
-      e.target.classList.add('active');
-    } else if (e.target.classList.contains('second')) {
-      slides.style.transform = 'translateX(-25%)';
-      e.target.classList.add('active');
-    } else if (e.target.classList.contains('third')) {
-      slides.style.transform = 'translatex(-50%)';
-      e.target.classList.add('active');
-    } else if (e.target.classList.contains('fourth')) {
-      slides.style.transform = 'translatex(-75%)';
-      e.target.classList.add('active');
+buttonsWrapper.addEventListener("click", (e) => {
+    if (e.target.nodeName === "BUTTON") {
+        Array.from(buttonsWrapper.children).forEach((item) =>
+            item.classList.remove("active")
+        );
+        if (e.target.classList.contains("first")) {
+            slides.style.transform = "translateX(-0%)";
+            e.target.classList.add("active");
+        } else if (e.target.classList.contains("second")) {
+            slides.style.transform = "translateX(-25%)";
+            e.target.classList.add("active");
+        } else if (e.target.classList.contains("third")) {
+            slides.style.transform = "translatex(-50%)";
+            e.target.classList.add("active");
+        } else if (e.target.classList.contains("fourth")) {
+            slides.style.transform = "translatex(-75%)";
+            e.target.classList.add("active");
+        }
     }
-  }
 });
